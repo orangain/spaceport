c23a67791d4b7a14ece87772c484ad762ca3a91b
orangain <orangain@gmail.com>
2018-02-22 23:16:27 +0900

    Create App component with React

---
+++ src/renderer/component/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export class App extends React.Component<{}, {}> {
+
+    render() {
+        return (
+            <div className="App">
+                Hello, Electron World!
+            </div>
+        )
+    }
+}
