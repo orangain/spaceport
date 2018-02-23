9fad8e0406dcef4d2562da14dee200ec6fc1df45
orangain <orangain@gmail.com>
2018-02-23 19:15:51 +0900

    Implement stopping process

--- src/renderer/component/App.tsx
+++ src/renderer/component/App.tsx
@@ -1,10 +1,19 @@
 import * as React from "react";
-import { exec } from "child_process";
+import { exec, ChildProcess } from "child_process";
+import "./App.css"
 
 export interface AppState {
     command: string;
     stdout: string;
     stderr: string;
+    log: string;
+    processState: ProcessState;
+}
+
+export enum ProcessState {
+    Stopped = 0,
+    Running,
+    Failed,
 }
 
 export class App extends React.Component<{}, AppState> {
@@ -13,33 +22,64 @@ export class App extends React.Component<{}, AppState> {
         command: `while true; do date; sleep 1s; done`,
         stdout: "",
         stderr: "",
+        log: "",
+        processState: ProcessState.Stopped,
     }
+
+    process: ChildProcess | null = null;
 
     constructor(props: {}, context?: any) {
         super(props, context);
-        this.executeScript = this.executeScript.bind(this);
-      }
+        this.startScript = this.startScript.bind(this);
+        this.stopScript = this.stopScript.bind(this);
+        this.restartScript = this.restartScript.bind(this);
+    }
 
-    executeScript() {
-        const process = exec(this.state.command);
+    startScript() {
+        this.setState({
+            stdout: "",
+            stderr: "",
+            log: "",
+            processState: ProcessState.Running,
+        });
 
-        process.stdout.on('data', (data) => {
+        this.process = exec(this.state.command);
+
+        this.process.stdout.on('data', (data) => {
             console.log(`stdout: ${data}`);
             this.setState({
                 stdout: this.state.stdout + data,
+                log: this.state.log + data,
             });
         });
 
-        process.stderr.on('data', (data) => {
+        this.process.stderr.on('data', (data) => {
             console.log(`stderr: ${data}`);
             this.setState({
                 stderr: this.state.stderr + data,
+                log: this.state.log + data,
             });
         });
 
-        process.on('close', (code) => {
+        this.process.on('close', (code) => {
             console.log(`child process exited with code ${code}`);
+
+            this.setState({
+                processState: ProcessState.Stopped,
+            });
         });
+    }
+
+    stopScript() {
+        if (this.process != null) {
+            this.process.kill();
+            this.process = null;
+        }
+    }
+
+    restartScript() {
+        this.stopScript();
+        this.startScript();
     }
 
     render() {
@@ -47,11 +87,13 @@ export class App extends React.Component<{}, AppState> {
             <div className="App">
                 <p>Hello, Electron World!</p>
                 <code>{this.state.command}</code>
-                <button type="button" onClick={this.executeScript}>実行</button>
-                stdout
-                <textarea value={this.state.stdout}/>
-                stderr
-                <textarea value={this.state.stderr}/>
+                <div>
+                {this.state.processState === ProcessState.Running ?
+                    <button type="button" onClick={this.stopScript}>停止</button> :
+                    <button type="button" onClick={this.startScript}>開始</button>}
+                </div>
+                log
+                <textarea value={this.state.log} />
             </div>
         )
     }
