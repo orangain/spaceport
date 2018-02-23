3a6c0d0f92436abd6011b4fb474cf01c597d6664
orangain <orangain@gmail.com>
2018-02-23 14:13:37 +0900

    Implement executing script

--- src/renderer/component/App.tsx
+++ src/renderer/component/App.tsx
@@ -1,11 +1,57 @@
 import * as React from "react";
+import { exec } from "child_process";
 
-export class App extends React.Component<{}, {}> {
+export interface AppState {
+    command: string;
+    stdout: string;
+    stderr: string;
+}
+
+export class App extends React.Component<{}, AppState> {
+
+    state = {
+        command: `while true; do date; sleep 1s; done`,
+        stdout: "",
+        stderr: "",
+    }
+
+    constructor(props: {}, context?: any) {
+        super(props, context);
+        this.executeScript = this.executeScript.bind(this);
+      }
+
+    executeScript() {
+        const process = exec(this.state.command);
+
+        process.stdout.on('data', (data) => {
+            console.log(`stdout: ${data}`);
+            this.setState({
+                stdout: this.state.stdout + data,
+            });
+        });
+
+        process.stderr.on('data', (data) => {
+            console.log(`stderr: ${data}`);
+            this.setState({
+                stderr: this.state.stderr + data,
+            });
+        });
+
+        process.on('close', (code) => {
+            console.log(`child process exited with code ${code}`);
+        });
+    }
 
     render() {
         return (
             <div className="App">
-                Hello, Electron World!
+                <p>Hello, Electron World!</p>
+                <code>{this.state.command}</code>
+                <button type="button" onClick={this.executeScript}>実行</button>
+                stdout
+                <textarea value={this.state.stdout}/>
+                stderr
+                <textarea value={this.state.stderr}/>
             </div>
         )
     }
