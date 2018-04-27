336c6df5117aff84d9ec87fe016a244cadbeb873
orangain <orangain@gmail.com>
2018-04-27 21:00:33 +0900

    Introduce fix-path package
    In macOS, PATH is not set as expected in GUI app. This will fix the problem.

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,5 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { App } from "./component/App";
+const fixPath = require("fix-path");
 
+fixPath(); // 環境変数PATHを設定する。
 ReactDOM.render(React.createElement(App, {}), document.getElementById("app"));
