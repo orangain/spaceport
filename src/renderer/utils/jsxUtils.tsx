1d25aa4bc10b6f1f71759cedd225dfa1eec91e67
orangain <orangain@gmail.com>
2018-05-05 11:27:01 +0900

    Refactor setting key property

--- src/renderer/utils/jsxUtils.tsx
+++ src/renderer/utils/jsxUtils.tsx
@@ -4,16 +4,23 @@ export function extendLogElements(
   logElements: React.ReactNode[],
   log: string
 ): React.ReactNode[] {
-  return [...logElements, toLogElements(log, logElements.length)];
+  const newElements = toLogElements(log).map((e, i) => {
+    if (!React.isValidElement(e)) {
+      return e;
+    }
+    return React.cloneElement(e as React.ReactElement<any>, { key: i });
+  });
+
+  return [...logElements, newElements];
 }
 
-function toLogElements(log: string, keyStartIndex: number): React.ReactNode[] {
+function toLogElements(log: string): React.ReactNode[] {
   let elements: React.ReactNode[] = [];
 
   const lineBreakRegex = /(\r\n|\n)/;
-  elements = log.split(lineBreakRegex).map((line, i) => {
+  elements = log.split(lineBreakRegex).map(line => {
     if (line.match(lineBreakRegex)) {
-      return <br key={keyStartIndex + i} />;
+      return <br />;
     } else {
       return line;
     }
